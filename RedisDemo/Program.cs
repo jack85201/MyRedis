dbd099d400ee44e6553cb470334024b4c7475125
jack85201 <862930873@qq.com>
2021-06-05 20:58:55 +0800

    MyRedis

--- RedisDemo/Program.cs
+++ RedisDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServiceStack.Redis;
 
 namespace RedisDemo
 {
@@ -11,6 +12,18 @@ namespace RedisDemo
     {
         static void Main(string[] args)
         {
+            using (IRedisClient client = new RedisClient("127.0.0.1",6379))
+            {
+                //删除当前数据库所有的KEY
+                //  client.FlushDb();
+                //添加字段
+                client.Set<int>("ID", 1);
+                client.Set<string>("name", "张三");
+                client.Set<int>("Depid", 666946);
+                //var shuchu = client.Get<string>("name");
+                Console.WriteLine("OK");
+                Console.ReadKey();
+            }
         }
     }
 }
